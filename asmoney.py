cdb1f895d1b8093033f6d5dadc6804a87e0b7bba
Andy Sciullo <andy.sciullo@gmail.com>
2016-09-01 23:21:57 -0500

    Added ability to read transactions and print to screen

--- asmoney.py
+++ asmoney.py
@@ -3,9 +3,58 @@ import os
 import re
 import openpyxl
 
-# Files must be named "*_Transactions.xlsx"
-# Workbooks should have all transactions in a sheet named "Journal"
+################################################################################
+# Description:
+#
+#  Arguments:
+#    1. dir      - Directory where transactions are stored. Make sure dir only
+#                  contains transactions (as *.xlsx) and no sub directories
+#    2. out_file - File name to write output data to
+#
+# ex. > python nano_fram_bfm.py ./dlog_dir out_file.csv
+#
+# Revision:
+#   0.1, Andy Sciullo, 09/01/2016, Initial Version
+#
+################################################################################
+################################################################################
+# Notes:
+#   - Files must be named "*_Transactions.xlsx"
+#   - Workbooks should have all transactions in a sheet named "Journal"
+#
+################################################################################
 
+################################################################################
+### Constants
+################################################################################
+COL_DATE = 1
+COL_RFNO = 2
+COL_MRCH = 3
+COL_ATYP = 4
+COL_ACNT = 5
+COL_ASUB = 6
+COL_AMNT = 7
+COL_DESC = 8
+
+################################################################################
+### Global Variables
+################################################################################
+dictDrDate = dict()
+dictDrMrch = dict()
+dictDrAtyp = dict()
+dictDrAcnt = dict()
+dictDrAsub = dict()
+dictDrAmnt = dict()
+dictDrDesc = dict()
+dictCrDate = dict()
+dictCrMrch = dict()
+dictCrAtyp = dict()
+dictCrAcnt = dict()
+dictCrAsub = dict()
+dictCrAmnt = dict()
+dictCrDesc = dict()
+arrDrRefNo = []
+arrCrRefNo = []
 
 ################################################################################
 # Function process_workbook
@@ -25,11 +74,68 @@ def process_workbook(wb_name):
     # Init variables
 
     # Search for files with correct name format
-    search_obj = re.search(r'.*_Transactions\.xlsx', wb_name)
-    if search_obj:
-        print "  Found Workbook: ", wb_name
-        #wb_temp = openpyxl.load_workbook(os.path.join(dir, wb_name))
-        #sheet = wb_temp.get_sheet_by_name('Journal')
+    print "  Processing Workbook: ", wb_name
+    wb_temp = openpyxl.load_workbook(os.path.join(dir, wb_name))
+    sheet = wb_temp.get_sheet_by_name('Journal')
+    for rowNum in range(2, sheet.max_row + 1):  # skip the first row
+        refNo = sheet.cell(row=rowNum, column=COL_RFNO).value
+        amnt = sheet.cell(row=rowNum, column=COL_AMNT).value
+        if amnt > 0:
+            if refNo in arrDrRefNo:
+                print "Duplicate reference number found on Debit side: ", refNo
+            else:
+                arrDrRefNo.append(refNo)
+            dictDrDate[refNo] = sheet.cell(row=rowNum, column=COL_DATE).value
+            dictDrMrch[refNo] = sheet.cell(row=rowNum, column=COL_MRCH).value
+            dictDrAtyp[refNo] = sheet.cell(row=rowNum, column=COL_ATYP).value
+            dictDrAcnt[refNo] = sheet.cell(row=rowNum, column=COL_ACNT).value
+            dictDrAsub[refNo] = sheet.cell(row=rowNum, column=COL_ASUB).value
+            dictDrAmnt[refNo] = sheet.cell(row=rowNum, column=COL_AMNT).value
+            dictDrDesc[refNo] = sheet.cell(row=rowNum, column=COL_DESC).value
+        else:
+            if refNo in arrCrRefNo:
+                print "Duplicate reference number found on Credit side: ", refNo
+            else:
+                arrCrRefNo.append(refNo)
+            dictCrDate[refNo] = sheet.cell(row=rowNum, column=COL_DATE).value
+            dictCrMrch[refNo] = sheet.cell(row=rowNum, column=COL_MRCH).value
+            dictCrAtyp[refNo] = sheet.cell(row=rowNum, column=COL_ATYP).value
+            dictCrAcnt[refNo] = sheet.cell(row=rowNum, column=COL_ACNT).value
+            dictCrAsub[refNo] = sheet.cell(row=rowNum, column=COL_ASUB).value
+            dictCrAmnt[refNo] = sheet.cell(row=rowNum, column=COL_AMNT).value
+            dictCrDesc[refNo] = sheet.cell(row=rowNum, column=COL_DESC).value
+
+################################################################################
+# Function process_workbook
+#
+# Description:
+#   Check the given file name for correct naming format and read contents for
+#   BFM data.
+#
+#  Arguments:
+#    1. file_name - Name of file to process.
+#
+#  Return Values:
+#    None
+#
+################################################################################
+def print_transactions():
+    for refNo in arrDrRefNo:
+        if refNo not in arrCrRefNo:
+            arrDrRefNo.remove(refNo)
+            print "Debit reference number not found for Credit: ", refNo
+
+    for refNo in arrCrRefNo:
+        if refNo not in arrCrRefNo:
+            arrCrRefNo.remove(refNo)
+            print "Credit reference number not found for Debit: ", refNo
+
+    for refNo in arrDrRefNo:
+        print("%s,%s,%s,%s,%s,%s,%s,%s" % (refNo, dictDrDate[refNo], dictDrMrch[refNo], dictDrAtyp[refNo], dictDrAcnt[refNo], dictDrAsub[refNo], \
+            dictDrAmnt[refNo], dictDrDesc[refNo]))
+        print("%s,%s,%s,%s,%s,%s,%s,%s" % (refNo, dictCrDate[refNo], dictCrMrch[refNo], dictCrAtyp[refNo], dictCrAcnt[refNo], dictCrAsub[refNo], \
+            dictCrAmnt[refNo], dictCrDesc[refNo]))
+
 
 ################################################################################
 # Main
@@ -62,9 +168,11 @@ print "Output File     : ", out_file
 #print "         Found Files: ", num_files
 for file_name in os.listdir(dir):
     if os.path.isfile(os.path.join(dir, file_name)):
-        process_workbook(file_name)
-        #sys.stdout.write("\r         Files Processed: %d" % file_count)
+        search_obj = re.search(r'.*_Transactions\.xlsx', file_name)
+        if search_obj:
+            process_workbook(file_name)
 
+print_transactions()
 # Close output file and report number of files processed
 #out_handle.close()
 #print "     Files processed: ", file_count
