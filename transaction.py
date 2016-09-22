0895bb22a2d1797cce6f5053df6766266136f732
Andy Sciullo <andy.sciullo@gmail.com>
2016-09-22 08:06:23 -0500

    Added transaction class

---
+++ transaction.py
@@ -0,0 +1,19 @@
+class TransRec:
+    recCnt = 0
+
+    def __init__(self):
+        self.refNo = ""
+        self.drDate = ""
+        self.drMrch = ""
+        self.drAtyp = ""
+        self.drAcnt = ""
+        self.drAsub = ""
+        self.drAmnt = 0
+        self.Desc = ""
+        self.crDate = ""
+        self.crMrch = ""
+        self.crAtyp = ""
+        self.crAcnt = ""
+        self.crAsub = ""
+        self.crAmnt = 0
+        self.crDesc = ""
